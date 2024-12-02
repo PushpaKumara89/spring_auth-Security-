470f68b35d3e995c9d12b7f0ec74da3562dd66b1
kpush <kpushpakumarajd@gmail.com>
2024-12-02 18:08:02 +0530

    CSRF, HttpServletRequest Session id, _csrf Token

--- README.md
+++
@@ -1,1 +0,0 @@
-# spring_auth-Security-
---
+++ src/main/java/com/appk/auth/HelloController.java
@@ -0,0 +1,27 @@
+package com.appk.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "hello  " + request.getSession().getId();
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return  (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestBody String name){
+        System.out.println(name);
+        return "saved";
+    }
+}
